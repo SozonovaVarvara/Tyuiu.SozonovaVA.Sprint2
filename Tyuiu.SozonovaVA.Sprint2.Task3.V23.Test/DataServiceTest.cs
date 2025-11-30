366fe77749a1e1cdbdce047cc1ed4595a7b89278
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-11-30 22:54:44 +0500

    Task3

---
+++ Tyuiu.SozonovaVA.Sprint2.Task3.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SozonovaVA.Sprint2.Task3.V23.Lib;
+namespace Tyuiu.SozonovaVA.Sprint2.Task3.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 18;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SozonovaVA.Sprint2.Task3.V23/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.SozonovaVA.Sprint2.Task3.V23.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Созонова В. А. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2"                                                                );
+        Console.WriteLine("* Тема: Вложенные операторы if - else"                                      );
+        Console.WriteLine("* Задание #3"                                                               );
+        Console.WriteLine("* Вариант #23"                                                              );
+        Console.WriteLine("* Выполнила: Созонова Варвара Андреевна | АСОиУб-25-1                      ");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:Написать программу, которая вычисляет требуемое значение функции Y с использованием " +
+            "вложенных оператор if-else, где пользователь вводит значение переменной X с клавиатуры. " +
+            "Округлить полученное значение до трех знаков после запятой;");
+        Console.WriteLine("");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine();
+        Console.WriteLine("Введите x:");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(x));
+    }
+}
