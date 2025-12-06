55fbe20ce049b33abfc443f50db1b6745df6cf85
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-12-06 20:22:02 +0500

    Task5

---
+++ Tyuiu.SozonovaVA.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.SozonovaVA.Sprint2.Task5.V3.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнила: Созонова В. А. | АСОиУб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #3                                                              *");
+Console.WriteLine("* Выполнила: Созонова Варвара Андреевна | АСОиУб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("X:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+string res = ds.FindDayName(x);
+
+Console.WriteLine(res);
+
+Console.ReadKey();
